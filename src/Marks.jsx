59427009a27147f92c54227192ca1a7848a02234
Shanmukh459 <saikrrish459@gmail.com>
2024-04-30 20:20:43 -0700

    rendering land instead of countries and using countries object for interiors

--- src/Marks.jsx
+++ src/Marks.jsx
@@ -1,13 +1,14 @@
 import { geoEqualEarth, geoPath } from "d3";
 
-
 const projection = geoEqualEarth();
 const path = geoPath(projection);
 
-export const Marks = ({data}) => (
+export const Marks = ({data:{land, interiors}}) => (
   <g className="marks">
-    {data.features.map((feature) => (
-      <path d={path(feature)} />
+    <path className="sphere" d={path({type:'Sphere'})} />
+    {land.features.map((feature) => (
+      <path className="land" d={path(feature)} />
     ))}
+    <path className="interiors" d={path(interiors)} />
   </g>
 )
--- src/useData.jsx
+++ src/useData.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 import { json } from "d3"
-import { feature } from "topojson"
+import { feature, mesh } from "topojson"
 
 const jsonUrl = 'https://unpkg.com/world-atlas@2.0.2/countries-50m.json'
 
@@ -8,8 +8,11 @@ export const useData = () => {
   const [data, setData] = useState(null)
   useEffect(() => {
     json(jsonUrl).then(topology => {
-      const {countries} = topology.objects
-      setData(feature(topology, countries))
+      const {countries, land} = topology.objects
+      setData({
+        land: feature(topology, land),
+        interiors: mesh(topology, countries, (a, b) => a !== b)
+      })
     })
   }, [])
   return data
