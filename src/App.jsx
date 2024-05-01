5839e94d39fc26d7e3842843427435ce5c2a8437
Shanmukh459 <saikrrish459@gmail.com>
2024-04-30 19:47:03 -0700

    data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import React, {useEffect, useState} from "react"
+import { useData } from "./useData"
+
+const height = 500
+const width = 960
+
 function App() {
-  return <h1>Hello!</h1>
+  const data = useData()
+  if(!data) {
+    return <h1>Loading...</h1>
+  }
+
+  console.log(data)
+  return (
+    <svg height={height} width={width} >
+      {/* {data.map()} */}
+    </svg>
+  )
 }
 
 export default App
---
+++ src/useData.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react"
+import { json } from "d3"
+import { feature } from "topojson"
+
+const jsonUrl = 'https://unpkg.com/world-atlas@2.0.2/countries-50m.json'
+
+export const useData = () => {
+  const [data, setData] = useState(null)
+  console.log(data)
+  useEffect(() => {
+    json(jsonUrl).then(topology => {
+      const {countries} = topology.objects
+      setData(feature(topology, countries))
+    })
+  }, [])
+}
